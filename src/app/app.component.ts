1e5b91e3b599e60de9188b6ec65cd63c4b0efc42
Brienne Kordis <blkordis@gmail.com>
2025-01-17 18:55:19 -0500

    add route to stash component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, MatIconModule, MatButtonModule, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StashComponent } from './stash/stash.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'stash',
+        component: StashComponent
+    }
+];
