934e413fb842711d13131a958239d97007f6dd2c
Brienne Kordis <blkordis@gmail.com>
2025-01-18 10:27:01 -0500

    add add-fabric component

---
+++ src/app/components/stash/add-fabric/add-fabric.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-fabric',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-fabric.component.html',
+  styleUrl: './add-fabric.component.css'
+})
+export class AddFabricComponent {
+
+}
