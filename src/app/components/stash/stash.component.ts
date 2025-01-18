a7bea99826dffbb4f79b665f7fc0a5cd8b8eb651
Brienne Kordis <blkordis@gmail.com>
2025-01-18 10:27:30 -0500

    update stash component

--- src/app/components/stash/stash.component.ts
+++ src/app/components/stash/stash.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-stash',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './stash.component.html',
   styleUrl: './stash.component.css'
 })
