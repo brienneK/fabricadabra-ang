0ec512caa1d4b4bf694476794b621d3167ca3ea9
Brienne Kordis <blkordis@gmail.com>
2025-01-17 19:02:01 -0500

    route to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,7 +10,7 @@ import { ToolbarComponent } from "./toolbar/toolbar.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, MatIconModule, MatButtonModule, ToolbarComponent],
+  imports: [RouterOutlet, HomeComponent, MatToolbarModule, MatIconModule, MatButtonModule, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
