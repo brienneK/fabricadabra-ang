7b04c1e6bebd317a3456d10a02bec0e3cbae269a
Brienne Kordis <blkordis@gmail.com>
2025-01-19 17:12:30 -0500

    add constructor

--- src/app/components/stash/add-fabric/add-fabric.component.ts
+++ src/app/components/stash/add-fabric/add-fabric.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-add-fabric',
-    imports: [],
-    templateUrl: './add-fabric.component.html',
-    styleUrl: './add-fabric.component.css'
+  selector: 'app-add-fabric',
+  imports: [],
+  templateUrl: './add-fabric.component.html',
+  styleUrl: './add-fabric.component.css',
 })
 export class AddFabricComponent {
-
+  constructor() {
+    console.log("I'm here!");
+  }
 }
