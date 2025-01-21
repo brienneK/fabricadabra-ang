fa1f405758934560f240eea3c11314fab258143e
Brienne Kordis <blkordis@gmail.com>
2025-01-21 00:32:44 -0500

    access fabrics in Stash

--- src/app/components/stash/stash.component.ts
+++ src/app/components/stash/stash.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { Fabric } from '@models/fabric.model';
+import { FabricService } from '@services/fabric.service';
 
 @Component({
   selector: 'app-stash',
@@ -8,4 +10,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './stash.component.html',
   styleUrl: './stash.component.scss',
 })
-export class StashComponent {}
+export class StashComponent {
+  fabricService = inject(FabricService);
+
+  fabrics: Signal<Fabric[]> = this.fabricService.fabrics;
+
+  // This is a computed property that filters the fabrics by the selected fiber.
+  filteredFabrics = computed(() => {});
+}
