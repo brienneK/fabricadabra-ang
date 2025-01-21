f6718875cbcb3c4b1fff572fa3a3d1160adde418
Brienne Kordis <blkordis@gmail.com>
2025-01-20 23:31:36 -0500

    add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,81 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { getAnalytics, logEvent } from 'firebase/analytics';
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from 'firebase/auth';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
+import { User } from '@models/user.model';
+import { FabricService } from './fabric.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  user = signal<User>(null);
+  isLoggedIn = computed(() => !!this.user());
+
+  fs = inject(getFirestore);
+  auth = inject(getAuth);
+  analytics = inject(getAnalytics);
+  router = inject(Router);
+  fabricService = inject(FabricService);
+
+  constructor() {
+    setPersistence(this.auth, browserLocalPersistence)
+      .then(async () => {
+        this.auth.onAuthStateChanged((firebaseUser) => {
+          if (!!firebaseUser) {
+            this.getUserDetails(firebaseUser.uid).then(
+              async (userData: Partial<User>) => {
+                const user = new User({
+                  id: firebaseUser.uid,
+                  email: firebaseUser.email,
+                  ...userData,
+                });
+                this.user.set(user);
+                this.fabricService.getFabrics(user.id);
+              }
+            );
+            return true;
+          } else {
+            this.user.set(null);
+            return false;
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('Error setting persistence', error);
+        logEvent(this.analytics, 'error', { message: error.message });
+      });
+  }
+
+  async getUserDetails(userId: string): Promise<User | null> {
+    const docRef = doc(this.fs, `users/${userId}`);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return new User(docSnap.data());
+    } else {
+      setDoc(docRef, {
+        userName: '',
+      });
+      return null;
+    }
+  }
+
+  async updateUser(changes: Partial<User>): Promise<void> {
+    const docRef = doc(this.fs, `users/${this.user().id}`);
+    return await setDoc(docRef, changes, { merge: true }).then(() => {
+      this.user.update((u) => ({
+        ...u,
+        ...changes,
+      }));
+    });
+  }
+
+  logout(): void {
+    this.auth.signOut().finally(() => this.router.navigateByUrl('/'));
+  }
+}
