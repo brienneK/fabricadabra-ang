06d945b2716716bc47c572917081badb8a3e7ab5
Brienne Kordis <blkordis@gmail.com>
2025-01-20 23:05:51 -0500

    prevent strict init

--- src/app/models/fabric.model.ts
+++ src/app/models/fabric.model.ts
@@ -1,12 +1,15 @@
-export interface FabricModel {
-    id: string;
-    fiber: string;
-    material: string;
-    pattern: string;
-    color: string;
-    source: string;
-    width: number;
-    length: number;
-    lengthType: 'yd.' | 'in.';
-    scrap: boolean;
+export class Fabric {
+  constructor(init?: Partial<Fabric>) {
+    Object.assign(this, init);
+  }
+  id: string;
+  fiber: string;
+  material: string;
+  pattern: string;
+  color: string;
+  source: string;
+  width: number;
+  length: number;
+  lengthType: 'yd.' | 'in.';
+  scrap: boolean;
 }
