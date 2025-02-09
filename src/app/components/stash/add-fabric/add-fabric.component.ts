3e1be3269f06d0d3e2102ec07f0f1f122e7a4e29
Brienne Kordis <blkordis@gmail.com>
2025-02-09 16:54:23 -0500

    build out add fabric component

--- src/app/components/stash/add-fabric/add-fabric.component.ts
+++ src/app/components/stash/add-fabric/add-fabric.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Fabric } from '@models/fabric.model';
+import { FabricService } from '@services/fabric.service';
+import { getAnalytics } from 'firebase/analytics';
+import { getStorage } from 'firebase/storage';
 
 @Component({
   selector: 'app-add-fabric',
-  imports: [FormsModule],
+  imports: [FormsModule, MatButtonModule],
   templateUrl: './add-fabric.component.html',
   styleUrl: './add-fabric.component.scss',
 })
-export class AddFabricComponent {}
+export class AddFabricComponent {
+  storage = inject(getStorage);
+  analytics = inject(getAnalytics);
+  fb = inject(FormBuilder);
+  fabricService = inject(FabricService);
+
+  addFabricForm = this.fb.group({
+    fiber: [''],
+    material: [''],
+    pattern: [''],
+    color: [''],
+    source: [''],
+    width: [0],
+    length: [0],
+    scrap: [false],
+  });
+
+  constructor() {}
+
+  onSubmit() {
+    this.addFabricForm.disable();
+    const val = this.addFabricForm.value;
+    const fabric: Partial<Fabric> = {
+      fiber: val.fiber,
+      material: val.material,
+      pattern: val.pattern,
+      color: val.color,
+      source: val.source,
+      width: val.width,
+      length: val.length,
+      scrap: val.scrap,
+    };
+    this.fabricService
+      .addFabric('userId', fabric)
+      .then(() => {
+        this.addFabricForm.enable();
+        this.addFabricForm.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
