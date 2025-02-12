7da0830efdcdc49d102def22349bce7b61764f80
Brienne Kordis <blkordis@gmail.com>
2025-02-11 22:34:25 -0500

    add logout functionality

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,4 +11,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  userService = inject(UserService);
+  logout = () => this.userService.logout();
+}
