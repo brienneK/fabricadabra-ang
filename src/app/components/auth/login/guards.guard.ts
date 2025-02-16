424d3e0ac16cfc333b935dfd2af3e0663ca997fa
Brienne Kordis <blkordis@gmail.com>
2025-02-16 18:22:00 -0500

    add guards

---
+++ src/app/components/auth/login/guards.guard.ts
@@ -0,0 +1,33 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const auth = inject(getAuth);
+
+  return new Promise((resolve) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(router.navigate(['/login']));
+      }
+    });
+  });
+};
+
+export const loggedInGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const auth = inject(getAuth);
+
+  return new Promise((resolve) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(router.navigate(['/account']));
+      } else {
+        resolve(true);
+      }
+    });
+  });
+};
