989b7d9b02229f144b0007136f15208bf1227340
Brie Kaese <blkordis@gmail.com>
2025-03-12 18:08:44 -0400

    add currency pipe

---
+++ src/app/shared/pipes/currency.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currency',
+  standalone: true,
+})
+export class CurrencyPipe implements PipeTransform {
+  transform(value: number): string {
+    return `${value < 0 ? '-' : ''}$${Math.abs(value).toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`;
+  }
+}
