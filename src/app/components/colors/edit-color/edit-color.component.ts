ce263a6080b8ec491bcc8bb59c5f218e4199ed80
Brie Kaese <blkordis@gmail.com>
2025-04-07 17:24:47 -0400

    fix edit color component

--- src/app/components/colors/edit-color/edit-color.component.ts
+++ src/app/components/colors/edit-color/edit-color.component.ts
@@ -1,108 +1,122 @@
-import { inject, signal, Signal, model, computed } from '@angular/core';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogConfig,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { Router } from '@angular/router';
 import { Color } from '@models/color.model';
-import { User } from '@models/user.model';
-import { SortingService } from '@services/sorting.service';
+import { ColorService } from '@services/color.service';
+import { DeleteDialogComponent } from '@shared/delete-dialog/delete-dialog.component';
 import { LoadingService } from '@shared/loading/loading.service';
-import { ColorStore } from '@store/color.store';
-import { UserStore } from '@store/user.store';
-import { AddColorComponent } from '../add-color/add-color.component';
 
 @Component({
   selector: 'app-edit-color',
   imports: [
     FormsModule,
-    MatButtonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule,
-    MatTooltipModule,
     MatSlideToggleModule,
-    MatTableModule,
-    MatSortModule,
-    MatListModule,
-    MatExpansionModule,
+    MatButtonModule,
   ],
   templateUrl: './edit-color.component.html',
   styleUrl: './edit-color.component.scss',
 })
 export class EditColorComponent {
-  protected readonly router = inject(Router);
-  protected readonly colorStore = inject(ColorStore);
-  protected readonly userStore = inject(UserStore);
-  protected readonly sorter = inject(SortingService);
-  protected readonly dialog = inject(MatDialog);
   protected readonly loading = inject(LoadingService);
+  protected readonly dialogRef = inject(MatDialogRef<EditColorComponent>);
+  protected readonly fb = inject(FormBuilder);
+  protected readonly colorService = inject(ColorService);
+  protected readonly dialog = inject(MatDialog);
   protected readonly snackBar = inject(MatSnackBar);
-  protected readonly panelOpenState = signal(false);
+  protected readonly data: { color: Color; userId: string } =
+    inject(MAT_DIALOG_DATA);
 
-  currentUser: Signal<User> = this.userStore.user;
-  #colors: Signal<Color[]> = this.colorStore.userColors;
+  #color = signal<Color>(this.data.color);
 
-  sortField = signal<string>('name');
-  sortAsc = signal<boolean>(true);
-
-  activeOnly = model<boolean>(true);
-  nameFilter = model<string>('');
-
-  filteredColors = computed(() => {
-    var colors = this.#colors().filter(
-      (m: Color) => m.active || m.active == this.activeOnly()
-    );
-    if (colors.length > 0) {
-      colors = this.sorter.sort(colors, this.sortField(), this.sortAsc());
-    }
-    return colors;
+  editColorForm = this.fb.group({
+    colorName: [this.data.color.name, Validators.required],
+    active: [this.data.color.active],
   });
 
-  sortColors(e: { active: string; direction: string }): void {
-    this.sortField.set(e.active);
-    this.sortAsc.set(e.direction == 'asc');
+  public get f() {
+    return this.editColorForm.controls;
   }
 
-  addColor(): void {
-    const dialogConfig: MatDialogConfig = {
-      data: this.currentUser().id,
+  onSubmit(): void {
+    this.editColorForm.disable();
+    const form = this.editColorForm.value;
+    const changes: Partial<Color> = {
+      name: form.colorName,
+      active: form.active,
     };
-    const dialogRef = this.dialog.open(AddColorComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe((success) => {
-      if (success) {
-        this.snackBar.open('Color added', 'OK');
+    this.loading.loadingOn();
+    this.colorService
+      .updateColor(this.data.userId, this.#color().id, changes)
+      .then((res) => {
+        if (res?.name === 'Error') {
+          this.snackBar.open(res.message, 'Close');
+          this.editColorForm.enable();
+        } else {
+          this.dialogRef.close({
+            success: true,
+            operation: 'saved',
+          });
+        }
+      })
+      .catch((err: Error) => {
+        console.log(err);
+        this.snackBar.open(
+          'Something went wrong - could not edit color.',
+          'Close'
+        );
+        this.editColorForm.enable();
+      })
+      .finally(() => this.loading.loadingOff());
+  }
+
+  deleteColor(): void {
+    const dialogConfig: MatDialogConfig = {
+      data: {
+        operation: 'Delete',
+        target: `color: ${this.#color().name}`,
+      },
+    };
+    const dialogRef = this.dialog.open(DeleteDialogComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe((confirm) => {
+      if (confirm) {
+        this.loading.loadingOn();
+        this.colorService
+          .deleteColor(this.data.userId, this.#color().id)
+          .then((res) => {
+            if (res?.name === 'Error') {
+              this.snackBar.open(res.message, 'Close');
+            } else {
+              this.dialogRef.close({
+                success: true,
+                operation: 'deleted',
+              });
+            }
+          })
+          .catch((err: Error) => {
+            this.snackBar.open(
+              'Something went wrong - could not delete color.',
+              'Close'
+            );
+          })
+          .finally(() => this.loading.loadingOff());
       }
     });
-  }
-
-  onClick(color: Color): void {
-    const dialogConfig: MatDialogConfig = {
-      data: { color: color, userId: this.currentUser().id },
-    };
-    const dialogRef = this.dialog.open(EditColorComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result.success) {
-        this.snackBar.open(`Color ${result.operation}`, 'OK');
-      }
-    });
-  }
-
-  showHelp(): void {
-    const dialogConfig: MatDialogConfig = {
-      disableClose: false,
-      maxWidth: '80vw',
-    };
-    // this.dialog.open(ColorHelpComponent, dialogConfig);
   }
 }
